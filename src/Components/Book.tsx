934c1a80cc871aae2cb681f55f7d06489a9d708e
mohammadhosseinesfandiari <mohammadhosseinesfandiari1@gmail.com>
2023-09-17 13:18:40 +0330

    setting book for testing

---
+++ src/Components/Book.tsx
@@ -0,0 +1,10 @@
+import { Paper } from "./Paper"
+
+
+export const Book:React.FC = ():JSX.Element => {
+  return (
+    <div id="book" className="book  relative w-[350px] h-[500px]">
+        <Paper/>
+    </div>
+  )
+}
